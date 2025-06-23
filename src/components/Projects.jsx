b8b66bd96cd0141a165a80ca91ca59968916709b
jagruthip2007 <jagruthipulumati@Jagruthis-MacBook-Pro.local>
2025-06-23 13:56:32 +0530

    changed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,4 @@
-import { motion } from 'framer-motion';
-import aboutImg from '../assets/about.png';
-import { PROJECTS } from './constants';
-import { LINKS } from './links';
+import { PROJECTS } from "./constants";
 
 const Projects = () => {
   return (
@@ -11,7 +8,9 @@ const Projects = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <h1 className="text-center text-4xl font-bold my-10 bg-gradient-to-b from-neutral-200 to-neutral-600 bg-clip-text text-transparent">Projects</h1>
+      <h1 className="text-center text-4xl font-bold my-10 bg-gradient-to-b from-neutral-200 to-neutral-600 bg-clip-text text-transparent">
+        Projects
+      </h1>
       <div className="flex flex-col gap-16 items-center">
         {PROJECTS.map((project, index) => (
           <motion.div
@@ -32,7 +31,7 @@ const Projects = () => {
                 {project.technologies.map((tech, index) => (
                   <span
                     key={index}
-                    className="mr-2 mb-2 rounded bg-white-400 px-2 py-1 text-sm font-medium text-purple-300 border-2 p-[2px] rounded-xl gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent ml-15 font-2xl font-bold"
+                    className="px-3 py-1 mr-2 mb-2 text-sm font-medium border border-purple-500 rounded-full bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 text-transparent bg-clip-text"
                   >
                     {tech}
                   </span>
@@ -41,7 +40,7 @@ const Projects = () => {
                   href={project.projectlink[0]}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-sm text-blue-400 underline ml-2 mr-2 mb-2 rounded bg-white-400 px-2 py-1 text-sm font-medium text-purple-300 border-2 p-[2px] rounded-xl gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                  className="px-3 py-1 ml-2 mb-2 text-sm font-medium text-blue-300 underline hover:text-blue-400 transition"
                 >
                   View Project
                 </a>
