8e673bbaa4837013a06ac46da49315e25a10e694
jagruthip2007 <jagruthipulumati@Jagruthis-MacBook-Pro.local>
2025-07-07 18:38:16 +0530

    chan

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 function App() {
   return (
-    <div className="relative min-h-screen w-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-blue">
-      {/* Full screen gradient background */}
-      <div className="fixed inset-0 -z-10 w-screen h-screen bg-gradient-to-br from-[#0f172a] via-[#0c1325] to-[#020617]" />
+    <div className="relative w-screen min-h-screen overflow-x-hidden text-white antialiased selection:bg-[#00c896] selection:text-black">
+      {/* Background glow gradient */}
+      <div className="fixed inset-0 -z-10 w-screen h-screen bg-gradient-to-br from-[#0d1117] via-[#0a0f1f] to-[#05070c]" />
 
       <Navbar />
       <Hero />
